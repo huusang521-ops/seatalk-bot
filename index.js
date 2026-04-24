dbbd106b7271150685ece2c56b4ff7590d37842b
huusang521-ops <huusang521@gmail.com>
2026-04-24 10:24:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -70,3 +70,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Bot running on port " + PORT);
 });
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
